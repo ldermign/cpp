b98a17cd6c0bcd0ac1635904dc6f32eb8ebd1455
Liena DERMIGNY <ldermign@student.42.fr>
2022-04-28 15:26:21 +0200

    correction numero de telephone

--- module_00/ex01/main.cpp
+++ module_00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ldermign <ldermign@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 11:21:57 by ldermign          #+#    #+#             */
-/*   Updated: 2022/04/27 20:27:57 by ldermign         ###   ########.fr       */
+/*   Updated: 2022/04/28 15:24:44 by ldermign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,6 +55,19 @@ void	print_phonebook(PhoneBook all) {
 	std::cout << std::endl;
 }
 
+int	is_digit(std::string str) {
+
+	int	length;
+
+	length = str.length();
+	for (int i = 0 ; i < length ; i++)
+	{
+		if (str[i] < 48 || str[i] > 57)
+			return -1;
+	}
+	return 0;
+}
+
 void	init_info(PhoneBook *all) {
 
 	std::cout << "You are going to add a new contact to your PhoneBook. What's his first name ?" << std::endl;
@@ -68,20 +81,21 @@ void	init_info(PhoneBook *all) {
 		std::getline (std::cin, all->repertory[all->it_contact].nickname);
 	std::cout << "Now, put his phone number." << std::endl;
 	while (all->repertory[all->it_contact].phone_number == "")
+	{
 		std::getline (std::cin, all->repertory[all->it_contact].phone_number);
-
-	// check phone number
-
+		if (all->repertory[all->it_contact].phone_number != "" && is_digit(all->repertory[all->it_contact].phone_number) == -1)
+		{
+			std::cout << "Error. Only digit in a phone number." << std::endl;
+			all->repertory[all->it_contact].phone_number = "";
+		}
+	}
 	std::cout << "Finaly, what's his darkest secret ?" << std::endl;
 	while (all->repertory[all->it_contact].darkest_secret == "")
 		std::getline (std::cin, all->repertory[all->it_contact].darkest_secret);
-	
-	// print_phonebook();
 }
 
-void	erase_if_full(Contact *to_erase)
-{
-	std::cout << "merde " << std::endl;
+void	erase_if_full(Contact *to_erase) {
+
 	to_erase->first_name = "";
 	to_erase->last_name = "";
 	to_erase->nickname = "";
@@ -89,11 +103,10 @@ void	erase_if_full(Contact *to_erase)
 	to_erase->darkest_secret = "";
 }
 
-void	add_user(PhoneBook *all_contacts)
-{
+void	add_user(PhoneBook *all_contacts) {
+
 	Contact		new_contact;
 
-	std::cout << "all_contacts->it_contact = " << all_contacts->it_contact << " --- all_contacts->full = " << all_contacts->full << std::endl;
 	if (all_contacts->full == 8 && all_contacts->it_contact == 8)
 		all_contacts->it_contact = 0;
 	if (all_contacts->full == 8)
@@ -104,25 +117,55 @@ void	add_user(PhoneBook *all_contacts)
 		all_contacts->full++;
 }
 
+void	print_welcome_message(void) {
+
+	std::cout << std::endl;
+	std::cout << "There is only 3 commands : \033[38;5;70mADD\033[0m, \033[38;5;75mSEARCH\033[0m and \033[38;5;160mEXIT\033[0m." << std::endl;
+	std::cout << "The first command, \033[38;5;70mADD\033[0m, let you add a contact in your repertory with some informations." << std::endl;
+	std::cout << "For \033[38;5;75mSEARCH\033[0m, you'll need to enter the index of the contact that you want to see a little more." << std::endl;
+	std::cout << "\033[38;5;160mEXIT\033[0m make you quit your PhoneBook. All data will be erased." << std::endl;
+	std::cout << "All others commands will be ignored." << std::endl;
+	std::cout << "You can now enter a command." << std::endl;
+}
+
+// void	search_user(PhoneBook *all_contacts) {
+
+// 	// Contact		new_contact;
+
+// 	// if (all_contacts->full == 8 && all_contacts->it_contact == 8)
+// 	// 	all_contacts->it_contact = 0;
+// 	// if (all_contacts->full == 8)
+// 	// 	erase_if_full(&(all_contacts->repertory[all_contacts->it_contact]));
+// 	// init_info(all_contacts);
+// 	// all_contacts->it_contact++;
+// 	// if (all_contacts->full != 8)
+// 	// 	all_contacts->full++;
+// }
+
 int	main()
 {
 	std::string	cmd_user;
 	PhoneBook	all_contacts;
 
+	std::cout << "\033[2J\033[1;1H";
+	std::cout << "~~~ Welcome to your PhoneBook ~~~" << std::endl;
+	std::cout << std::endl << "Here, you can add any contact of your choice with a few informations." << std::endl;
 	all_contacts.it_contact = 0;
 	all_contacts.full = 0;
 	while (42)
 	{
-		print_phonebook(all_contacts);
+		print_welcome_message();
+		// print_phonebook(all_contacts);
 		std::getline (std::cin, cmd_user);
 		if (cmd_user == "EXIT")
 			exit(0);
 		else if (cmd_user == "ADD")
 			add_user(&all_contacts);
-		// else if (cmd_user == "SEARCH")
-
-			// std::cout << cmd_user << std::endl;
-		
+		else if (cmd_user == "SEARCH")
+		{
+			print_phonebook(all_contacts);
+			// search_user(&all_contacts);
+		}
 	}
 
 	return 0;
