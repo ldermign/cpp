c6af86e38918769f678cb824318976765d4bd0f4
Liena DERMIGNY <ldermign@student.42.fr>
2022-04-30 14:57:16 +0200

    time fait

--- module_00/ex02/Account.cpp
+++ module_00/ex02/Account.cpp
@@ -6,11 +6,12 @@
 /*   By: ldermign <ldermign@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/29 13:01:29 by ldermign          #+#    #+#             */
-/*   Updated: 2022/04/29 15:10:42 by ldermign         ###   ########.fr       */
+/*   Updated: 2022/04/30 14:56:19 by ldermign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <ctime>
 #include "Account.hpp"
 
 int	Account::_nbAccounts = 0;
@@ -52,7 +53,37 @@ int	Account::getNbWithdrawals( void ) {
 	return Account::_totalNbWithdrawals;
 }
 
-static void	displayAccountsInfos( void ) {
+static void	_displayTimestamp( void ) {
+
+	time_t t = time(NULL);
+
+	tm* timePtr = localtime(&t);
+	std::cout << "[";
+	std::cout << timePtr->tm_year + 1900;
+	if (timePtr->tm_mon + 1 < 10)
+		std::cout << "0";
+	std::cout << timePtr->tm_mon + 1;
+	if (timePtr->tm_mday + 1 < 10)
+		std::cout << "0";
+	std::cout << timePtr->tm_mday;
+	std::cout << "_";
+	if (timePtr->tm_hour + 1 < 10)
+		std::cout << "0";
+	std::cout << timePtr->tm_hour;
+	if (timePtr->tm_min + 1 < 10)
+		std::cout << "0";
+	std::cout << timePtr->tm_min;
+	if (timePtr->tm_sec + 1 < 10)
+		std::cout << "0";
+	std::cout << timePtr->tm_sec;
+	std::cout << "] ";
+}
+
+void	Account::displayAccountsInfos( void ) {
+
+	std::cout << "[" << Account::_displayTimestamp() << "] " 
+		<< "index:" << Account::_accountIndex << ";amount:" 
+		<< Account::_amount << ";created" << std::endl;
 
 	return ;
 }
