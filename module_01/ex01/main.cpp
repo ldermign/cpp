6ddfa2a5fcdb3bd74e6db6d8e30adaf999623f81
Liena DERMIGNY <ldermign@student.42.fr>
2022-05-04 16:10:19 +0200

    m01ex01 correction check taille horde

--- module_01/ex01/main.cpp
+++ module_01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ldermign <ldermign@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 17:20:39 by ldermign          #+#    #+#             */
-/*   Updated: 2022/05/04 15:13:10 by ldermign         ###   ########.fr       */
+/*   Updated: 2022/05/04 16:09:13 by ldermign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,11 @@ int main() {
 	int	nb_zombie = 3;
 	
 	Zombie *newHorde = zombieHorde(nb_zombie, "ldermign");
+	if (newHorde == NULL)
+	{
+		std::cout << "Error size Horde." << std::endl;
+		return 0;
+	}
 
 	for (int i = 0 ; i < nb_zombie ; i++)
 		newHorde[i].announce();
--- module_01/ex01/zombieHorde.cpp
+++ module_01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: ldermign <ldermign@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 17:24:39 by ldermign          #+#    #+#             */
-/*   Updated: 2022/05/04 15:13:57 by ldermign         ###   ########.fr       */
+/*   Updated: 2022/05/04 16:08:50 by ldermign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 Zombie* zombieHorde( int N, std::string name ) {
 
+	if (N <= 0)
+		return NULL;
 	Zombie *ptrHorde = new Zombie[N];
 	for (int i = 0 ; i < N ; i++)
 		ptrHorde[i].setNameZombie(name);
