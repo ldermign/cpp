7fd09df8457c564ca9c61bc8f6772eabb5ba02a4
Liena DERMIGNY <ldermign@e1r8p15.clusters.42paris.fr>
2022-05-06 19:16:17 +0200

    m01ex04 - ajouter saut de ligne quand il faut + renommer le nouveau fichier

--- module_01/ex04/main.cpp
+++ module_01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ldermign <ldermign@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 14:57:38 by ldermign          #+#    #+#             */
-/*   Updated: 2022/05/06 18:41:21 by ldermign         ###   ########.fr       */
+/*   Updated: 2022/05/06 19:15:45 by ldermign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ Of course, handle unexpected inputs and errors. You have to create and turn in y
 own tests to ensure your program works as expected.
 */
 
-void	replace_strings(std::string string_file, std::string s1, std::string s2) {
+void	replace_strings(std::ofstream& new_file, std::string string_file, std::string s1, std::string s2) {
 
 	std::string	new_string = string_file;
 	int			len_str = new_string.length();
@@ -36,47 +36,43 @@ void	replace_strings(std::string string_file, std::string s1, std::string s2) {
 		if (new_string.substr(i, s1.length()) == s1) {
 			new_string = new_string.substr(0, i);
 			new_string += s2 + &string_file[i + s1.length()];
-			std::cout << "new string = [" << new_string << "]" << std::endl;
 		}
 		len_str = new_string.length();
 	}
+	new_file << new_string;
 }
-
-/*
-int main ()
-{
-  std::string str="We think in generalities, but we live in details.";
-                                           // (quoting Alfred N. Whitehead)
-
-  std::string str2 = str.substr (3,5);     // "think"
-
-  std::size_t pos = str.find("live");      // position of "live" in str
-
-  std::string str3 = str.substr (pos);     // get from "live" to the end
-
-  std::cout << str2 << ' ' << str3 << '\n';
-
-  return 0;
-}
-*/
 
 int	change_string(const char *file, std::string s1, std::string s2) {
 (void)s2;
-	std::fstream	ifs;
+	std::ifstream	ifs;
+	std::ofstream	new_file;
 	std::string		string_file;
 
-	ifs.open (file, std::fstream::in);
+	ifs.open (file, std::ifstream::in);
 	if (ifs.fail()) {
 		std::cout << file << ": No such file or directory" << std::endl;
+		return EXIT_FAILURE;
+	}
+	new_file.open ("NEW_ONE_TMP", std::ofstream::out | std::ios::ate);
+	if (new_file.fail()) {
+		std::cout << "Error: can't create new file" << std::endl;
 		return EXIT_FAILURE;
 	}
 	int line = 1;
 	while (std::getline(ifs, string_file)) {
 		if (string_file.find(s1, 0) != std::string::npos)
-			replace_strings(string_file, s1, s2);
+			replace_strings(new_file, string_file, s1, s2);
+		else
+			new_file << string_file;
+		/*
+		Si pas a la fin du fichier, ajouter saut de ligne
+		// if ()
+			// new_file << std::endl;
+		*/
 		line++;
 	}
 	ifs.close();
+	new_file.close();
 	return EXIT_SUCCESS;
 }
 
@@ -88,5 +84,8 @@ int main(int ac, char **av) {
 	}
 	else if (change_string(av[1], av[2], av[3]) == EXIT_FAILURE)
 		return EXIT_FAILURE;
+	/*
+	renommer le nouveau fichier avec l'ancien nom
+	*/
 	return 0;
 }
