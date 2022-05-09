3a7726707c8e126d38d811d3440ef4d71247ef54
Liena DERMIGNY <ldermign@e1r10p17.42.fr>
2022-05-09 16:07:50 +0200

    m02ex00 - debut exercice

--- module_02/ex00/Fixed.cpp
+++ module_02/ex00/Fixed.cpp
@@ -6,10 +6,29 @@
 /*   By: ldermign <ldermign@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/08 17:31:58 by ldermign          #+#    #+#             */
-/*   Updated: 2022/05/08 17:32:36 by ldermign         ###   ########.fr       */
+/*   Updated: 2022/05/09 15:11:17 by ldermign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Fixed.hpp"
 
+Fixed::Fixed( void ) {
+
+	return ;
+}
+
+Fixed & Fixed::operator=( const & rhs ) {
+
+	return ;
+}
+
+Fixed::Fixed( Fixed const & i ) {
+
+	return ;
+}
+
+Fixed::~Fixed( void ) {
+
+	return ;
+}
--- module_02/ex00/Fixed.hpp
+++ module_02/ex00/Fixed.hpp
@@ -6,11 +6,25 @@
 /*   By: ldermign <ldermign@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/08 17:32:38 by ldermign          #+#    #+#             */
-/*   Updated: 2022/05/08 17:33:06 by ldermign         ###   ########.fr       */
+/*   Updated: 2022/05/09 15:08:50 by ldermign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 # define FIXED_HPP
+
+class Fixed {
+
+public:
+
+	Fixed( void );
+	Fixed & operator=( const & rhs );
+	Fixed( Fixed const & i)
+	~Fixed( void );
+
+private:
+
+
+};
 
 #endif
