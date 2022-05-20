9c9f415dcd0b55b9457a2ac22392f10d6a98d8ec
Liena DERMIGNY <ldermign@e3r10p19.clusters.42paris.fr>
2022-05-20 15:08:01 +0200

    m05ex00 - debut

---
+++ module_05/ex00/Bureaucrat.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldermign <ldermign@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/20 14:14:38 by ldermign          #+#    #+#             */
+/*   Updated: 2022/05/20 15:07:26 by ldermign         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <iostream>
+
+class Bureaucrat {
+
+public:
+
+// CANONICAL FORM
+
+	Bureaucrat( void );
+	~Bureaucrat( void );
+
+private:
+	/* data */
+
+};
+
+#endif
