a03b82b82d58b58482af0769e6fb1827f9e70fb3
Liena DERMIGNY <ldermign@e1r5p17.clusters.42paris.fr>
2022-05-22 14:51:36 +0200

    r

--- module_05/ex00/Bureaucrat.cpp
+++ module_05/ex00/Bureaucrat.cpp
@@ -6,19 +6,45 @@
 /*   By: ldermign <ldermign@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 15:06:05 by ldermign          #+#    #+#             */
-/*   Updated: 2022/05/20 15:07:37 by ldermign         ###   ########.fr       */
+/*   Updated: 2022/05/22 14:40:33 by ldermign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
+std::string	Bureaucrat::getName( void ) const { return this->_name; }
+int	Bureaucrat::getGrade( void ) const { return this->_grade; }
+
+
+
 /*
 **	CANONICAL FORM
 */
 
-Bureaucrat::Bureaucrat( void ) {
+Bureaucrat::Bureaucrat( void ) {}
+
+Bureaucrat &Bureaucrat::operator=( Bureaucrat const &rhs ) {
+	
+	if (&rhs != this) {}
+
+	return *this;
+	
 }
 
-Bureaucrat::~Bureaucrat( void ) {
+Bureaucrat::Bureaucrat( Bureaucrat const &src ) {
+	
+	*this = src;
+	
 }
 
+Bureaucrat::~Bureaucrat( void ) {}
+
+/*
+**	OPERATOR OVERLOAAD
+*/
+
+std::ostream &operator<<( std::ostream &o, Bureaucrat const &rhs ) {
+
+	o << rhs.getName();
+	return o;
+}
--- module_05/ex00/Bureaucrat.hpp
+++ module_05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: ldermign <ldermign@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 14:14:38 by ldermign          #+#    #+#             */
-/*   Updated: 2022/05/20 15:07:26 by ldermign         ###   ########.fr       */
+/*   Updated: 2022/05/22 14:39:27 by ldermign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,39 @@ class Bureaucrat {
 
 public:
 
+	std::string			getName( void ) const;
+	int					getGrade( void ) const;
+
+	void				IncrementGrade( void );
+	void				DecrementGrade( void );
+
 // CANONICAL FORM
 
 	Bureaucrat( void );
+	Bureaucrat &operator=( Bureaucrat const &rhs );
+	Bureaucrat( Bureaucrat const &src );
 	~Bureaucrat( void );
 
 private:
-	/* data */
+
+	std::string	const	_name;
+	int					_grade;
 
 };
 
+std::ostream & operator<<( std::ostream & o, Bureaucrat const & rhs );
+
 #endif
+
+/*
+Toute tentative d’instancier un Bureaucrat en utilisant un échelon invalide jettera une
+exception :
+Bureaucrat::GradeTooHighException ou Bureaucrat::GradeTooLowException.
+
+Vous ajouterez des accesseurs pour chacun de ces attributs : getName() et getGrade().
+Implémentez aussi deux fonctions membres afin d’incrémenter ou de décrémenter
+l’échelon du bureaucrate. Si ce dernier est trop haut ou trop bas,
+les mêmes exceptions que dans le constructeur seront jetées.
+Rappelez-vous. Puisque l’échelon 1 est le plus élevé, et 150 le plus
+bas, incrémenter un échelon 3 donnera l’échelon 2 au bureaucrate.
+*/
