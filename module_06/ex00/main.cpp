89c026cb1b1c183d5e5d16381a452600be55833f
ldermign <ldermign@student.42.fr>
2022-06-01 21:04:17 +0200

    m06ex00 - end

--- module_06/ex00/main.cpp
+++ module_06/ex00/main.cpp
@@ -6,19 +6,33 @@
 /*   By: ldermign <ldermign@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 15:27:47 by ldermign          #+#    #+#             */
-/*   Updated: 2022/06/01 16:15:14 by ldermign         ###   ########.fr       */
+/*   Updated: 2022/06/01 21:03:29 by ldermign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Conversion.hpp"
 
-void	printPoint0( char const c ) {
+void	printPoint0( char const c, char const *str ) {
 
-	for (int i = 0 ; i < str.size() ; i++)
+	std::string	tmp = str;
+
+	if (c != 'F' && c != 'D')
+		return ;
+	int pos = tmp.find('.');
+	if (pos == -1)
+		return;
+	int ret = pos;
+	pos++;
+	while (str[pos] && str[pos] == '0')
+		pos++;
+	if (str[pos] == '\0')
+		std::cout << &str[ret];
+	if (str[ret] == '.' && !str[ret + 1])
+		std::cout << "0";
 }
 
-void	print( Conversion const toConv ) {
+void	print( Conversion const toConv, const char *str ) {
 
 	{
 		std::cout << "char: ";
@@ -42,7 +56,7 @@ void	print( Conversion const toConv ) {
 			std::cout << toConv.getImpFloat();
 		else {
 			std::cout << toConv.getFloat();
-			printPoint0(toConv.getWhichConv());
+			printPoint0(toConv.getWhichConv(), str);
 		}
 		if (toConv.getImpFloat() != "-inf" && toConv.getImpFloat() != "impossible"
 			&& toConv.getImpFloat() != "+inf")
@@ -54,8 +68,9 @@ void	print( Conversion const toConv ) {
 		if (toConv.getImpDouble() != "")
 			std::cout << toConv.getImpDouble() << std::endl;
 		else {
-			std::cout << toConv.getDouble() << std::endl;
-			printPoint0(toConv.getWhichConv());
+			std::cout << toConv.getDouble();
+			printPoint0(toConv.getWhichConv(), str);
+			std::cout << std::endl;
 		}
 	}
 }
@@ -72,7 +87,7 @@ int main( int ac, char **av ) {
 		toConv.getConv(av[1]);
 		if (toConv.getWhichConv() != 'N')
 			toConv.doConv();
-		print(toConv);
+		print(toConv, av[1]);
 	}
 	return 0;
 }
