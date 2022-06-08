1f91b3487b4be564eebfc84b949f40c300c057e4
Liena DERMIGNY <ldermign@e1r11p2.42.fr>
2022-06-08 13:54:58 +0200

    m07ex01 - more test in main

--- module_07/ex01/main.cpp
+++ module_07/ex01/main.cpp
@@ -6,12 +6,25 @@
 /*   By: ldermign <ldermign@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 14:33:16 by ldermign          #+#    #+#             */
-/*   Updated: 2022/06/07 15:24:48 by ldermign         ###   ########.fr       */
+/*   Updated: 2022/06/08 13:43:33 by ldermign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "iter.hpp"
+
+// class Awesome
+// {
+// 	public:
+// 	Awesome( void ) : _n( 42 ) { return; }
+// 	int get( void ) const { return this->_n; }
+// 	private:
+// 	int _n;
+// };
+
+// std::ostream & operator<<( std::ostream & o, Awesome const & rhs ) { o << rhs.get(); return o; }
+// template< typename T >
+// void print( T const & x ) { std::cout << x << std::endl; return; }
 
 int main( void ) {
 
@@ -33,6 +46,14 @@ int main( void ) {
 		std::string test[] = {"Salut ", "ca ", "va ", "?"};
 		iter(test, 4, print);
 	}
+	
+
+	// int tab[] = { 0, 1, 2, 3, 4 };
+	// Awesome tab2[5];
+
+	// iter( tab, 5, print );
+	// iter( tab2, 5, print );
+	
 	
 	return 0;
 }
