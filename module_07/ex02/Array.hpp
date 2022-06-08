0a5dcc1997871c59b010376f58e3fe09d298c240
Liena DERMIGNY <ldermign@e1r11p2.42.fr>
2022-06-08 11:54:11 +0200

    m07ex02 - end

--- module_07/ex02/Array.hpp
+++ module_07/ex02/Array.hpp
@@ -6,49 +6,59 @@
 /*   By: ldermign <ldermign@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 15:28:10 by ldermign          #+#    #+#             */
-/*   Updated: 2022/06/07 15:46:16 by ldermign         ###   ########.fr       */
+/*   Updated: 2022/06/08 11:53:29 by ldermign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
 
+#include <strings.h>
+
+template< typename T >
 class Array {
 
 private:
 
-	/* data */
+	unsigned int	_size;
+	T				*_array;
 
 public:
 
-	int size( void ) const;
+	unsigned int size( void ) const {
+		return this->_n;
+	};
+	Array( unsigned int n ) : _size(n), _array(new T[n]) {}
 
-	Array( unsigned int n );
+	class InvalidIndex : public std::exception {
+	public:
+		virtual const char	*what( void ) const throw() {
+			return ("\033[38;5;124mIndex invalid\033[0m\n");
+		}	
+	};
 
-	Array( void );
-	Array &operator=( Array const &rhs );
-	Array( Array const &src );
-	~Array( void );
+	T	&operator[]( unsigned int x ) {
+		if (x < 0 || x >= this->_size )
+			throw Array::InvalidIndex();
+		return this->_array[x];
+	}
+
+	Array( void ) : _size(0), _array(new T[0]) {}
+	Array &operator=( Array const &rhs ) {
+		if (&rhs != this)
+			for (int i = 0 ; i < this->_n ; i++)
+				this->_array[i] = rhs._array[i]; 
+		return *this;
+	}
+	Array( Array const &src ) {
+		*this = src;
+	}
+	~Array( void ) {
+		delete [] this->_array;
+	};
 
 };
 
-Array::Array( void ) {
 
-}
-
-Array &Array::operator=( Array const &rhs ) { (void)rhs;
-
-	return *this;
-}
-
-Array::Array( Array const &src ) {
-	
-	*this = src;
-
-}
-
-Array::~Array( void ) {
-
-}
 
 #endif
--- module_07/ex02/main.cpp
+++ module_07/ex02/main.cpp
@@ -6,13 +6,93 @@
 /*   By: ldermign <ldermign@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 15:27:24 by ldermign          #+#    #+#             */
-/*   Updated: 2022/06/07 15:33:58 by ldermign         ###   ########.fr       */
+/*   Updated: 2022/06/08 11:53:46 by ldermign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include "Array.hpp"
 
 int main( void ) {
+
+	std::cout << "Array of strings:" << std::endl;
+
+	{
+		int						n = 4;
+		Array< std::string >	test(n);
+		test[0] = "Salut";
+		test[1] = "ca ";
+		test[2] = "va ";
+		test[3] = "?";
+
+		try {
+			for (int i = 0 ; i < n ; i++)
+				std::cout << test[i] << std::endl;
+		}
+		catch (const std::exception &e) {
+			std::cerr << e.what() << '\n';
+		}
+	}
+
+	std::cout << std::endl  << "Array of int:" << std::endl;
+
+	{
+		int				n = 5;
+		Array< int >	test(n);
+		test[0] = 42;
+		test[1] = -8;
+		test[2] = 7;
+		test[3] = 2147483647;
+		test[4] = 0;
+		
+		try {
+			for (int i = 0 ; i < n ; i++)
+				std::cout << test[i] << std::endl;
+		}
+		catch (const std::exception &e) {
+			std::cerr << e.what() << '\n';
+		}
+
+	}
+
+	std::cout << std::endl << "Array of char:" << std::endl;
+
+	{
+		int				n = 8;
+		Array< char >	test(n);
+		test[0] = 'l';
+		test[1] = 'd';
+		test[2] = 'e';
+		test[3] = 'r';
+		test[4] = 'm';
+		test[5] = 'i';
+		test[6] = 'g';
+		test[7] = 'n';
+		
+		try {
+			for (int i = 0 ; i < n ; i++)
+				std::cout << test[i] << std::endl;
+		}
+		catch (const std::exception &e) {
+			std::cerr << e.what() << '\n';
+		}
+	}
+
+	std::cout << std::endl << "---------- Errors ----------" << std::endl;
+
+	{
+		std::cout << std::endl << "Wrong index:" << std::endl;
+
+		int				n = 3;
+		Array< char >	test(n);
+		
+		try {
+			std::cout << test[42] << std::endl;
+		}
+		catch (const std::exception &e) {
+			std::cerr << e.what() << '\n';
+		}
+	}
 
 	return 0;
 }
