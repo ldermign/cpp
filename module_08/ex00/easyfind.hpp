1f063e169e41ddf013565d380f0df5f4503a17e0
Liena DERMIGNY <ldermign@e1r11p2.42.fr>
2022-06-08 16:09:52 +0200

    m08ex00 - start

---
+++ module_08/ex00/easyfind.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldermign <ldermign@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/08 16:07:15 by ldermign          #+#    #+#             */
+/*   Updated: 2022/06/08 16:09:35 by ldermign         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+template< typename T >
+void	easyfind( T a, int nbr ) { (void)T;(void)nbr;
+
+
+}
+
+#endif
---
+++ module_08/ex00/main.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldermign <ldermign@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/08 16:06:20 by ldermign          #+#    #+#             */
+/*   Updated: 2022/06/08 16:08:43 by ldermign         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main( void ) {
+
+	return 0;
+}
+
+/*
+Commençons tranquillement avec un premier exercice facile.
+Écrivez une fonction template easyfind acceptant un type T. 
+Elle prend deux paramètres. Le premier est de type T et le 
+second un nombre entier.
+
+Partant du principe que T est un container d’entiers, cette fonction doit 
+trouver la première occurrence du second paramètre dans le premier paramètre.
+Si aucune occurrence n’a été trouvée, vous pouvez soit jeter une exception, 
+soit retourner une valeur d’erreur de votre choix. Si vous avez besoin 
+d’inspiration, jetez un oeil au comportement des containers standards.
+
+Bien entendu, implémentez et rendez vos propres tests pour démontrer que tout
+marche comme attendu.
+
+Vous n’avez pas à gérer les containers associatifs.
+
+*/
