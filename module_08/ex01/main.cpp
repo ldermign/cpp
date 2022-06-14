81efe7f1faa4bd9b8518ea252c992dfbf79e634a
Liena DERMIGNY <ldermign@e1r11p18.42.fr>
2022-06-14 13:56:57 +0200

    m08ex01 - one error

--- module_08/ex01/main.cpp
+++ module_08/ex01/main.cpp
@@ -6,28 +6,12 @@
 /*   By: ldermign <ldermign@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 15:24:14 by ldermign          #+#    #+#             */
-/*   Updated: 2022/06/14 13:55:22 by ldermign         ###   ########.fr       */
+/*   Updated: 2022/06/14 13:56:20 by ldermign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Span.hpp"
-
-/*
-
-Bien sûr, implémentez vos propres tests qui devront être bien plus complets que celui
-donné ci-dessous. Testez votre Span avec au moins 10 000 nombres. Vous pouvez tester
-avec plus de nombres, c’est encore mieux.
-
-Le meilleur pour la fin. Une possibilité qu’il serait utile d’avoir, c’est celle de pouvoir
-remplir votre Span en utilisant une plage d’itérateurs (range of iterators). Faire des
-centaines d’appels à addNumber(), c’est plutôt répétitif. Implémentez une fonction qui
-permet d’ajouter plusieurs nombres à votre Span en un appel.
-
-Si vous ne voyez pas quoi faire, étudiez les Containers. Certaines
-fonctions membres prennent une plage d’itérateurs afin d’ajouter une
-séquence d’éléments au container.
-*/
 
 int	main( void ) {
 
